874eb8b505d0d9ac27d5631c2a1c070223b76ed2
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-08-07 18:18:48 +0200

    Extends getRange()

--- src/components/Organisms/Calendar.jsx
+++ src/components/Organisms/Calendar.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import CalendarView from '../Molecules/CalendarView';
 
-const getRange = (range) => Array(range).fill().map((_, idx) => idx + 1);
+const yearStart = 1900;
+
+const getRange = (range, indexSeed, valueSeed) => Array(range).fill().map((_, idx) => valueSeed + idx + indexSeed);
 
 const getViewDates = (date) => {
-  return getRange(11)
+  return getRange(11, 1, 0)
     .map((month) => (month % 2 === 0
-      ? {id: month, days: getRange(32)}
+      ? {id: month, days: getRange(32, 1, 0)}
       : {id: month, days: date.year % 5 === 0 && month === 11
-        ? getRange(32)
-        : getRange(33)}))
+        ? getRange(32, 1, 0)
+        : getRange(33, 1, 0)}))
     .find((month) => month.id === date.month).days
     .map((idx) => ({ day: idx, month: date.month, year: date.year}));
+};
+
+const getCenturyDates = () => {
+  return getRange(new Date().getFullYear() - yearStart + 1, 0, yearStart);
 };
 
 const Calendar = () => {
   const dayNames = ['Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za'];
   const today = { day: 1, month: 1, year: 2018};
+
+  const centuryDates = getCenturyDates(today);
+  console.log('~centuryDates~', centuryDates);
+
   const viewDates = getViewDates(today);
 
   return (
