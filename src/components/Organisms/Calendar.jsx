ab3465a9a016534a195089c14ec3da81d077d3b6
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-08-11 12:43:29 +0200

    refine calendar

--- src/components/Organisms/Calendar.jsx
+++ src/components/Organisms/Calendar.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import CalendarView from '../Molecules/CalendarView';
 import {getDisplayDays} from '../../utility/calendar';
 
-const Calendar = () => {
-  const today = {
-    year: 2018,
-    monthOfYear: 8,
-    dateOfTheMonth: 7,
-  };
+const today = {
+  year: 2018,
+  monthOfYear: 8,
+  dateOfTheMonth: 7,
+};
 
-  return (
-    <CalendarView displayDays={getDisplayDays(today)} />
-  );
-}
+const Calendar = () => <CalendarView displayDays={getDisplayDays(today)} />;
 
 export default Calendar;
