cab7ba6621a330f2f69a89d844400bcf8ac3a677
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-10-10 13:51:08 +0200

    Updates

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-// Picks up all Storybook stories under ../src/stories
-
-import { configure } from '@storybook/react';
-
-function loadStories() {
-  require('../src/stories');
-}
-
-configure(loadStories, module);
--- src/utility/number.js
+++
@@ -1,3 +0,0 @@
-// number utility to get a range of numbers
-
-export const getRange = ({range, indexSeed, valueSeed}) => Array(range).fill().map((_, index) => valueSeed + index + indexSeed);
